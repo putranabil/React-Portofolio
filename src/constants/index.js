60a9234c6633775671970721568bfc344369443a
putranabil <rahmadona4343@gmail.com>
2024-10-09 20:01:34 -0700

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,80 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Perkenalkan, nama saya Putra Nabil. Saya adalah seorang pelajar
+ rekayasa perangkat lunak (RPL) di salah satu SMK di kota Bandung yang bernama SMK MedikaCom .
+ Yang sangat tertarik dengan dunia pemrograman dan pengembangan aplikasi. Saya suka sekali 
+ memecahkan masalah kompleks menjadi solusi yang sederhana melalui kode.
+ Tujuan saya adalah menciptakan inovasi teknologi yang dapat bermanfaat bagi banyak orang`;
+
+export const ABOUT_TEXT = ``;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - Present",
+    role: "Staf IT Sistem Informasi Manajemen (SIM)",
+    company: "SMK Medikacom",
+    description: `Melakukan Pengolahan Data Internal Sekolah & Melakukan Troubleshooting dan Maintenance Sistem Aplikasi Sekolah`,
+    technologies: ["PHP", "Native", "HTML", "MySQL"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Jl. Anatomi No.2 Komp. Dosen UNPAD, Cigadung, Cibeunying Kaler, Kota Bandung ",
+  phoneNo: "+62 812 9802 1258 ",
+  email: "rheyfauzan@gmail.com",
+};
